5487529a698b2c8787364425b14c73e61aa7ab04
Viktor Turskyi <koorchik@gmail.com>
2013-08-17 12:38:40 +0300

    Fixed bug with filters pipelining

---
+++ t/04-custom_filters.js
@@ -0,0 +1,48 @@
+var LIVR = require('../lib/LIVR');
+
+LIVR.Validator.registerDefaultRules({
+    my_trim: function() {
+        return function(value, undefined, outputArr) {
+            if (value === undefined || value === null || typeof value === 'object' || value === '' ) return;
+
+            value += '';
+            outputArr.push( value.replace(/^\s*/, '').replace(/\s*$/, '') );
+        };
+    },
+    my_lc: function(field) {
+        return function(value, undefined, outputArr) {
+            if (value === undefined || value === null || typeof value === 'object' || value === '' ) return;
+
+            value += '';
+            outputArr.push( value.toLowerCase() );
+        };
+    },
+    my_ucfirst: function(field) {
+        return function(value, undefined, outputArr) {
+            if (value === undefined || value === null || typeof value === 'object' || value === '' ) return;
+
+            value += '';
+            outputArr.push( value.charAt(0).toUpperCase() + value.slice(1) );
+        };
+    }
+});
+
+test('Validate data with registered rules', function() {
+    var validator = new LIVR.Validator({
+        word1: ['my_trim', 'my_lc', 'my_ucfirst'],
+        word2: ['my_trim', 'my_lc'],
+        word3: ['my_ucfirst']
+    });
+
+    var output = validator.validate({
+        word1: ' wordOne ',
+        word2: ' wordTwo ',
+        word3: 'wordThree '
+    });
+
+    deepEqual( output, {
+        word1: 'Wordone',
+        word2: 'wordtwo',
+        word3: 'WordThree '
+    }, 'Should appluy changes to values' );
+});
--- t/testrunner.js
+++ t/testrunner.js
@@ -2,5 +2,10 @@ var testrunner = require("qunit");
 
 testrunner.run([{
     code: "../lib/LIVR.js",
-    tests: "test_suite.js"
-}]);
+    tests: "01-test_suite.js"
+},
+{
+    code: "../lib/LIVR.js",
+    tests: "04-custom_filters.js"
+}
+]);
